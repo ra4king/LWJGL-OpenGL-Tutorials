f0816ff780509c92628e5f0e97078e20fc1fe80a
ra4king <devnull@localhost>
2012-10-14 01:03:44 -0400

    Setup for Example9.3

--- src/main/java/com/ra4king/opengl/arcsynthesis/gl33/chapter9/example3/Example9_3.java
+++ src/main/java/com/ra4king/opengl/arcsynthesis/gl33/chapter9/example3/Example9_3.java
@@ -1,5 +1,23 @@
 package com.ra4king.opengl.arcsynthesis.gl33.chapter9.example3;
 
-public class Example9_3 {
+import com.ra4king.opengl.GLProgram;
+
+public class Example9_3 extends GLProgram {
+	public static void main(String[] args) {
+		new Example9_3().run(true);
+	}
 	
+	public Example9_3() {
+		super("Example 9.3", 500, 500, false);
+	}
+	
+	@Override
+	public void init() {
+		
+	}
+	
+	@Override
+	public void render() {
+		
+	}
 }
