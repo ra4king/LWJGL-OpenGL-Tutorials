3bd42596f3076afdd47e9a48a8de9753ab45ab10
ra4king <devnull@localhost>
2013-01-20 20:40:43 -0500

    Added Vector2

---
+++ src/main/java/com/ra4king/opengl/util/math/Vector2.java
@@ -0,0 +1,142 @@
+package com.ra4king.opengl.util.math;
+
+import java.nio.FloatBuffer;
+
+import org.lwjgl.BufferUtils;
+
+public class Vector2 {
+	private float x, y;
+	
+	public Vector2() {
+		set(0, 0);
+	}
+	
+	public Vector2(float v) {
+		this(v, v);
+	}
+	
+	public Vector2(float x, float y) {
+		set(x, y);
+	}
+	
+	public Vector2(Vector2 vec) {
+		set(vec);
+	}
+	
+	public Vector2(Vector3 vec) {
+		set(vec);
+	}
+	
+	public Vector2(Vector4 vec) {
+		set(vec);
+	}
+	
+	public float x() {
+		return x;
+	}
+	
+	public Vector2 x(float x) {
+		this.x = x;
+		return this;
+	}
+	
+	public float y() {
+		return y;
+	}
+	
+	public Vector2 y(float y) {
+		this.y = y;
+		return this;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(o instanceof Vector2) {
+			Vector2 v = (Vector2)o;
+			return x == v.x && y == v.y;
+		}
+		
+		return false;
+	}
+	
+	public Vector2 set(float x, float y) {
+		this.x = x;
+		this.y = y;
+		return this;
+	}
+	
+	public Vector2 set(Vector2 vec) {
+		return set(vec.x, vec.y);
+	}
+	
+	public Vector2 set(Vector3 vec) {
+		return set(vec.x(), vec.y());
+	}
+	
+	public Vector2 set(Vector4 vec) {
+		return set(vec.x(), vec.y());
+	}
+	
+	public Vector2 reset() {
+		x = y = 0;
+		return this;
+	}
+	
+	public float length() {
+		return (float)Math.sqrt(x * x + y * y);
+	}
+	
+	public Vector2 normalize() {
+		float length = length();
+		x /= length;
+		y /= length;
+		return this;
+	}
+	
+	public float dot(Vector2 vec) {
+		return x * vec.x + y * vec.y;
+	}
+	
+	public Vector2 add(float x, float y) {
+		this.x += x;
+		this.y += y;
+		return this;
+	}
+	
+	public Vector2 add(Vector2 vec) {
+		return add(vec.x, vec.y);
+	}
+	
+	public Vector2 sub(float x, float y) {
+		this.x -= x;
+		this.y -= y;
+		return this;
+	}
+	
+	public Vector2 sub(Vector2 vec) {
+		return sub(vec.x, vec.y);
+	}
+	
+	public Vector2 mult(float f) {
+		return mult(f, f);
+	}
+	
+	public Vector2 mult(float x, float y) {
+		this.x *= x;
+		this.y *= y;
+		return this;
+	}
+	
+	public Vector2 mult(Vector2 vec) {
+		return mult(vec.x, vec.y);
+	}
+	
+	private final static FloatBuffer direct = BufferUtils.createFloatBuffer(2);
+	
+	public FloatBuffer toBuffer() {
+		direct.clear();
+		direct.put(x).put(y);
+		direct.flip();
+		return direct;
+	}
+}
