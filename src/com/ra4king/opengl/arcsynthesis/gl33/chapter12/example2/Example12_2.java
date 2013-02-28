5d3da932cf54dd2cc53f4fe4e2b58e4cfe0d2a63
Roi <Roi@ROI-ATALLA>
2013-02-28 01:28:34 -0500

    Setup Example 12.2.

---
+++ src/com/ra4king/opengl/arcsynthesis/gl33/chapter12/example2/Example12_2.java
@@ -0,0 +1,23 @@
+package com.ra4king.opengl.arcsynthesis.gl33.chapter12.example2;
+
+import com.ra4king.opengl.GLProgram;
+
+public class Example12_2 extends GLProgram {
+	public static void main(String[] args) {
+		new Example12_2().run(true);
+	}
+	
+	public Example12_2() {
+		super("Example 12.1 - HDR Lighting", 800, 800, true);
+	}
+	
+	@Override
+	public void init() {
+		
+	}
+	
+	@Override
+	public void render() {
+		
+	}
+}
